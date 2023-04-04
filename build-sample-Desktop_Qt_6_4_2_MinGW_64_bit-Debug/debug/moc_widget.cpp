159c2e70b0f3183794bbec66dcdb23c046d1496f
MLrollIT <1009158157@qq.com>
2023-04-04 12:10:25 +0800

    更新

--- build-sample-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/debug/moc_widget.cpp
+++ build-sample-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/debug/moc_widget.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../need a timer/widget.h"
+#include "../../stage1/widget.h"
 #include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
